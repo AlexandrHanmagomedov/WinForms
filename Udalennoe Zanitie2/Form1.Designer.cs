c3a9bc712460d2ac681c50d02fd3681eefecc054
Snejoke <98012287+AlexandrHanmagomedov@users.noreply.github.com>
2022-11-04 21:59:35 +0300

    Add files via upload

---
+++ Udalennoe Zanitie2/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Udalennoe_Zanitie2 {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose ( bool disposing ) {
+            if ( disposing && ( components != null ) ) {
+                components.Dispose ( );
+            }
+            base.Dispose ( disposing );
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent ( ) {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Load = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(685, 396);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(509, 414);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(71, 24);
+            this.Load.TabIndex = 1;
+            this.Load.Text = "Load";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(602, 414);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(71, 24);
+            this.Save.TabIndex = 2;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 471);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.Button Save;
+    }
+}
+
---
+++ Udalennoe Zanitie2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Udalennoe_Zanitie2 {
+    public partial class Form1 : Form {
+        public Form1 ( ) {
+            InitializeComponent ( );
+        }
+
+        private void Load_Click ( object sender, EventArgs e ) {
+            OpenFileDialog open = new OpenFileDialog ( );
+            //// создали экземпляр установим фильтр для файлов
+            open.Filter = "All Files(*.*)|*.*|Text Files\r\n( *.txt )|*.txt"; 
+            open.FilterIndex = 1;// по умолчанию фильтруются текстовые файлы
+            if ( open.ShowDialog ( ) == DialogResult.OK ) {
+                StreamReader reader = File.OpenText ( open.FileName );
+                textBox1.Text = reader.ReadToEnd ( );// считываем файл до конца 
+                reader.Close ( ); // закрываем reader
+            }
+        }
+
+        private void Save_Click ( object sender, EventArgs e ) {
+            SaveFileDialog save = new SaveFileDialog ( );
+            // создали экземпляр
+            if ( save.ShowDialog ( ) == DialogResult.OK ) {
+                StreamWriter writer = new StreamWriter ( save.FileName );
+                // записываем в файл содержимое поля
+                writer.Write ( textBox1.Text );
+                writer.Close ( );
+                // закрываем writer
+            }
+        }
+    }
+}
