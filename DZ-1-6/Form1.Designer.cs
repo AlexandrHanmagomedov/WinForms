0f45eca3ba38c3fcd3d1d88d7e0234cf35f6b9bc
Snejoke <98012287+AlexandrHanmagomedov@users.noreply.github.com>
2022-11-11 00:27:14 +0300

    Add files via upload

---
+++ DZ-1-6/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace DZ_1_6 {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose ( bool disposing ) {
+            if ( disposing && ( components != null ) ) {
+                components.Dispose ( );
+            }
+            base.Dispose ( disposing );
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent ( ) {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(328, 40);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(139, 20);
+            this.inputTextBox.TabIndex = 6;
+            this.inputTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(291, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Определить по введенной дате  день недели.";
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(328, 108);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(139, 20);
+            this.resultTextBox.TabIndex = 8;
+            this.resultTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(364, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Результат";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(348, 155);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(92, 23);
+            this.btnOk.TabIndex = 10;
+            this.btnOk.Text = "ОК";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+    }
+}
+
---
+++ DZ-1-6/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace DZ_1_6 {
+    public partial class Form1 : Form {
+        public Form1 ( ) {
+            InitializeComponent ( );
+            btnOk.Click += OkClick;
+
+        }
+
+        private void OkClick ( object sender, EventArgs e ) {
+            if ( inputTextBox.Text == "" ) {
+                resultTextBox.Text = dateTimePicker.Value.Date.ToString ( "dddd" ).ToUpper ( );//ru
+                inputTextBox.Text = dateTimePicker.Value.ToShortDateString ( );
+            }
+            else {
+                try {
+                    DateTime day = new DateTime ( );
+                    day = DateTime.Parse ( inputTextBox.Text );
+                    resultTextBox.Text = day.ToString ( "dddd" ).ToUpper ( );
+                    dateTimePicker.Value = day;
+                }
+                catch ( Exception ) {
+                    
+                }
+            }
+        }
+    }
+}
