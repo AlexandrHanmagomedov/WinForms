1e6a018772291e5ea25649e97a9557f9f233c53f
Snejoke <98012287+AlexandrHanmagomedov@users.noreply.github.com>
2022-11-20 17:52:15 +0300

    Домашнее задание 6

---
+++ DZ-6-3 Динамическое добавление меню,подменю/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Динамическое_добавление_меню_подменю {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose ( bool disposing ) {
+            if ( disposing && ( components != null ) ) {
+                components.Dispose ( );
+            }
+            base.Dispose ( disposing );
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent ( ) {
+            this.TopLevelMenu = new System.Windows.Forms.TextBox();
+            this.SubItem = new System.Windows.Forms.TextBox();
+            this.ButtonAdMenu = new System.Windows.Forms.Button();
+            this.ButtonAdSubMenu = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TopLevelMenu
+            // 
+            this.TopLevelMenu.Location = new System.Drawing.Point(12, 71);
+            this.TopLevelMenu.Name = "TopLevelMenu";
+            this.TopLevelMenu.Size = new System.Drawing.Size(110, 20);
+            this.TopLevelMenu.TabIndex = 1;
+            this.TopLevelMenu.TextChanged += new System.EventHandler(this.TopLevelMenu_TextChanged);
+            // 
+            // SubItem
+            // 
+            this.SubItem.Location = new System.Drawing.Point(12, 139);
+            this.SubItem.Name = "SubItem";
+            this.SubItem.Size = new System.Drawing.Size(110, 20);
+            this.SubItem.TabIndex = 2;
+            this.SubItem.TextChanged += new System.EventHandler(this.TopLevelMenu_TextChanged);
+            // 
+            // ButtonAdMenu
+            // 
+            this.ButtonAdMenu.Enabled = false;
+            this.ButtonAdMenu.Location = new System.Drawing.Point(12, 29);
+            this.ButtonAdMenu.Name = "ButtonAdMenu";
+            this.ButtonAdMenu.Size = new System.Drawing.Size(110, 36);
+            this.ButtonAdMenu.TabIndex = 3;
+            this.ButtonAdMenu.Text = "Add menu item";
+            this.ButtonAdMenu.UseVisualStyleBackColor = true;
+            this.ButtonAdMenu.Click += new System.EventHandler(this.ButtonAddMenu_Click);
+            // 
+            // ButtonAdSubMenu
+            // 
+            this.ButtonAdSubMenu.Enabled = false;
+            this.ButtonAdSubMenu.Location = new System.Drawing.Point(12, 97);
+            this.ButtonAdSubMenu.Name = "ButtonAdSubMenu";
+            this.ButtonAdSubMenu.Size = new System.Drawing.Size(110, 36);
+            this.ButtonAdSubMenu.TabIndex = 4;
+            this.ButtonAdSubMenu.Text = "Add submenu item";
+            this.ButtonAdSubMenu.UseVisualStyleBackColor = true;
+            this.ButtonAdSubMenu.Click += new System.EventHandler(this.ButtonAddSubMenu_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(139, 181);
+            this.Controls.Add(this.ButtonAdSubMenu);
+            this.Controls.Add(this.ButtonAdMenu);
+            this.Controls.Add(this.SubItem);
+            this.Controls.Add(this.TopLevelMenu);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dynamic menu addition";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox TopLevelMenu;
+        private System.Windows.Forms.TextBox SubItem;
+        private System.Windows.Forms.Button ButtonAdMenu;
+        private System.Windows.Forms.Button ButtonAdSubMenu;
+    }
+}
+
---
+++ DZ-6-3 Динамическое добавление меню,подменю/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace Динамическое_добавление_меню_подменю {
+    public partial class Form1 : Form {
+
+        private readonly MenuStrip topMenu = new MenuStrip ( );
+        public Form1 ( ) {
+            InitializeComponent ( );
+            CenterToScreen ( );
+            this.MainMenuStrip = topMenu;
+            this.Controls.Add ( topMenu );
+        }
+
+        private void TopLevelMenu_TextChanged ( object sender, EventArgs e ) {
+            ButtonAdMenu.Enabled = TopLevelMenu.Text.Length > 0;
+            ButtonAdSubMenu.Enabled = ( TopLevelMenu.Text.Length > 0 && SubItem.Text.Length > 0 );
+        }
+
+        private void ButtonAddMenu_Click ( object sender, EventArgs e ) {
+            if ( CheckTopLevelMenu ( TopLevelMenu.Text, out int index ) == false ) {
+                ToolStripMenuItem newMenu = ( ToolStripMenuItem ) topMenu.Items.Add ( TopLevelMenu.Text );
+                newMenu.Click += ToolStripMenuItem_Click;
+            }
+
+            else
+                MessageBox.Show ( "This menu item already exists.", "info", MessageBoxButtons.OK, MessageBoxIcon.Information );
+
+        }
+
+        private void ButtonAddSubMenu_Click ( object sender, EventArgs e ) {
+            if ( CheckTopLevelMenu ( TopLevelMenu.Text, out int index ) == false ) {
+                MessageBox.Show ( "This menu item already exists.", "info", MessageBoxButtons.OK, MessageBoxIcon.Information );
+            }
+            else {
+                if ( CheckSubMenu ( SubItem.Text, ( ToolStripMenuItem ) topMenu.Items[index] ) == true ) {
+                    MessageBox.Show ( "This submenu already exists in the menu" + TopLevelMenu.Text, "info", MessageBoxButtons.OK, MessageBoxIcon.Information );
+                }
+                else {
+                    ToolStripMenuItem newSubMenu = ( ToolStripMenuItem ) topMenu.Items[index];
+                    newSubMenu.DropDownItems.Add ( SubItem.Text );
+                }
+            }
+        }
+
+        private void ToolStripMenuItem_Click ( object sender, EventArgs e ) => TopLevelMenu.Text = ( sender as ToolStripMenuItem ).Text;
+
+        private bool CheckTopLevelMenu ( string text, out int index ) 
+        {
+            index = -1;
+            for ( int i = 0; i < topMenu.Items.Count; i++ ) {
+                if ( topMenu.Items[i].Text == text ) {
+                    index = i;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool CheckSubMenu ( string text, ToolStripMenuItem menu ) 
+        {
+            for ( int i = 0; i < menu.DropDownItems.Count; i++ ) {
+                if ( menu.DropDownItems[i].Text == text ) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
